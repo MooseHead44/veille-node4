6aeafc7ddc72dca2182b782303ebb697eef95ecb
Pelletier <e1552599@cmaisonneuve.qc.ca>
2018-02-08 09:34:09 -0500

    changement dans le code

--- 02_app.js
+++ 02_app.js
@@ -1,65 +1,29 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const fs = require ('fs');
 app.use(express.static('public'));
 
-/*------------------------Route formulaire------------------------------*/
-app.get('/formulaire', function (req, res) {
- console.log(__dirname);
- res.sendFile( __dirname + "/public/html/" + "01_form.htm" );
-})
-
-/*------------------------Route membres------------------------------*/
-app.get('/membres', function (req, res) {
- console.log(__dirname);
- fs.readFile (__dirname + '/public/data/membres.txt', 'utf-8', (err, data) => {
- 	if (err) throw err;
- 	let liste = JSON.parse(data);
- 	let html = "<!DOCTYPE html>";
-    html+= "<html>";
-    html+= "<head>";
-    html+= "<style>table{background-color:tomato; color:white; border: solid 1px solid} td{border:dashed 1px white}</style>";
-    html+= "</head>";
-    html+="<table>"
-    html+="<thead><tr><th>Les membres</th></tr></thead>";
-    html+="<tbody>";
-
-    for(poulet in liste){
-        html += "<tr>";
-        html += "<td>";
-        html += liste[poulet].nom
-        html += "</td>";
-
-        html+= "<tr>";
-        html += "<td>";
-        html += liste[poulet].telephone
-        html += "</td>";
-
-        html+= "<tr>";
-        html += "<td>";
-        html += liste[poulet].courriel
-        html += "</td>";
-
-        html+= "<tr>";
-        html += "<td>";
-        html += liste[poulet].id
-        html += "</td>";
+const transforme_en_tableau = (collection) =>{
+    let chaine = '<table>'
+    for(elm of collection){
+        for(p in elm){
+             chaine += '<tr><td>' + p + '</td><td>' + collection[p] + '</td></tr>'; 
+        }
     }
-
-    html += "</tbody></table></style>";
-
- 	res.end(data);
- })
- 
+    chaine += '</table>'
+    return chaine
+}
+//////////////////////////////////////////////////////////////////////
+app.get('/formulaire', function (req, res) { 
+ console.log(__dirname);
+ res.sendFile( __dirname + "/html/" + "01_form.htm" );
 })
-
-/*------------------------Route /------------------------------*/
+//////////////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
  console.log('accueil')
  res.end('<h1>Accueil</h1>')
 })
-
-/*------------------------Route /traiter_get------------------------------*/
+//////////////////////////////////////////////////////////////////////
 app.get('/traiter_get', function (req, res) {
  // Preparer l'output en format JSON
 
@@ -68,28 +32,31 @@ console.log('la route /traiter_get')
 // on utilise l'objet req.query pour récupérer les données GET
  let reponse = {
  prenom:req.query.prenom,
- nom:req.query.nom,
- courriel:req.query.courriel,
- telephone:req.query.telephone
+ nom:req.query.nom
  };
+console.log(reponse);
+res.end(JSON.stringify(reponse));
 
- fs.readFile (__dirname + '/public/data/membres.txt', 'utf-8', (err, data) => {
- 	if (err) throw err;
- 	//Enregistre en tableau
- 	let liste = JSON.parse(data);
- 	//Ajoute la réponse
- 	liste.push(reponse);
- 	fs.writeFile (__dirname + '/public/data/membres.txt', JSON.stringify (liste), 'utf-8', (err) => {
- 		if (err) throw err;
- 		res.end(JSON.stringify (liste));
- 	})
- });
+fs.appendFile('unfichier.txt', ',' + JSON.stringify(reponse), function(err){
+    if(err) throw err;
+    console.log('Sauvegarder');
+    
+});
+
+
 })
+//////////////////////////////////////////////////////////////////////
+app.get('membres', (req, res) => {
+    fs.readFile('... membres.txt', 'utf8', function (err, data){
+        if(err) throw err;
+        let collection = JSON.parse('[' + data + ']');
+        res.end(transforme_en_tableau(collection))
+    });
 
-let server = app.listen(8080, function () {
- let host = server.address().address
- let port = server.address().port
- 
- console.log("Exemple l'application écoute sur http://%s:%s", host, port)
+})
+    var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
 
+    console.log("Exemple l'application écoute sur http://%s:%s", host, port);
 })
